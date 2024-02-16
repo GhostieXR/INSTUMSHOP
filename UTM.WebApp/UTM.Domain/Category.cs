a72f03f06a73a059c2b23e20f73a434a16b8483e
danchudin <danchudin@gmail.com>
2024-02-16 23:29:15 +0200

    Добавил модели

---
+++ UTM.WebApp/UTM.Domain/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace UTM.Domain
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UTM.WebApp/UTM.Domain/User.cs
@@ -0,0 +1,15 @@
+﻿namespace UTM.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
