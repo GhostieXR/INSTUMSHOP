940f9afafd9f61a7059127bc41db1e567b6cf5cf
yaroslavdavidov03 <yaroslavdavidov03@gmail.com>
2024-03-14 12:30:38 +0200

    Добавил дбконтекст

---
+++ UTM.WebApp/UTM.Database/WatchStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using UTM.Domain;
+
+namespace UTM.Database
+{
+    public class WatchStoreContext : DbContext
+    {
+        public WatchStoreContext() : base("name=WatchStoreContext")
+        {
+        }
+
+        public DbSet<Watch> Watches { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
