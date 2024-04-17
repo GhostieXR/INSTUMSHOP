8ce784d8077d937fa47872dacf242e08815bb1bb
danchudin <danchudin@gmail.com>
2024-04-17 07:39:18 +0300

    Добавил страницу часов

---
+++ UTM.WebApp/Controllers/WatchController.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using UTM.Database;
+using UTM.Domain;
+using UTM.WebApp.Attr;
+
+namespace UTM.WebApp.Controllers
+{
+    [Auth("Moderator", "Admin")]
+    public class WatchController : Controller
+    {
+        private WatchStoreContext db = new WatchStoreContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Watches.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Watch watch = db.Watches.Find(id);
+            if (watch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(watch);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WatchId,Name,Brand,Price,ImageUrl")] Watch watch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Watches.Add(watch);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(watch);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Watch watch = db.Watches.Find(id);
+            if (watch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(watch);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WatchId,Name,Brand,Price,ImageUrl")] Watch watch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(watch).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(watch);
+        }
+
+        [Auth("Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Watch watch = db.Watches.Find(id);
+            if (watch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(watch);
+        }
+
+        [Auth("Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Watch watch = db.Watches.Find(id);
+            db.Watches.Remove(watch);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UTM.WebApp/UTM.BusLogic/PasswordHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UTM.BusLogic
+{
+    public class PasswordHelper
+    {
+        public static string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                provider.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password + salt);
+                byte[] hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
