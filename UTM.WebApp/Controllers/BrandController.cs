e7488193f2630ffe204311b71fe796802829c644
yaroslavdavidov03 <yaroslavdavidov03@gmail.com>
2024-04-30 08:35:46 +0300

    Добавил бренды

---
+++ UTM.WebApp/Controllers/BrandController.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using UTM.Database;
+using UTM.Domain;
+using UTM.WebApp.Attr;
+
+namespace UTM.WebApp.Controllers
+{
+    public class BrandController : Controller
+    {
+        private WatchStoreContext db = new WatchStoreContext();
+
+        [Auth("Moderator", "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.Brands.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BrandId,Name")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brand);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BrandId,Name")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+        
+        [Auth("Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Auth("Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Brand brand = db.Brands.Find(id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
